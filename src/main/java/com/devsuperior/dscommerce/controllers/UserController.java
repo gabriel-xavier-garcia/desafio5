45f80e1fc59fcec7ab7530fa26cd00c1680d4453
Gabriel Garcia <gabrielproaj@gmail.com>
2025-10-08 18:41:16 -0300

    ProductMinDTO lista paginada customizada

--- src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.UserDTO;
-import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
